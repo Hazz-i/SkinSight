054199a8ddb1e51a05b85b4fde44e57dc2ddce54
Hazz-i <wahidh697@gmail.com>
2025-07-28 08:05:42 +0700

    update: name file TeamSection

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,5 @@ export { default as Navbar } from './Navbar';
 export { default as HeroSection } from './HeroSection';
 export { default as FeaturesSection } from './FeaturesSection';
 export { default as DownloadSection } from './DownloadSection';
-export { default as Footer } from './Footer';
 export { default as TeamSection } from './TeamSection';
+export { default as Footer } from './Footer';
